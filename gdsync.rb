eb52883eba49ccad010c389cf3e62268f4960963
kbinani <kbinani.bt@gmail.com>
2016-06-18 17:15:58 +0900

    Update --help

--- gdsync.rb
+++ gdsync.rb
@@ -16,21 +16,21 @@ opt[:archive] = false
 
 parser = OptionParser.new
 parser.banner = 'Usage: gdsync [options] SRC DEST'
-parser.on('--delete', 'delete extraneous files from dest dirs') { |v| opt[:delete] = true }
-parser.on('-c', '--checksum', 'skip based on checksum, not mod-time & size') { |v| opt[:checksum] = true }
 parser.on('-v', '--verbose', 'increase verbosity') { |v| opt[:verbose] = true }
-parser.on('-n', '--dry-run', 'show what would have been transferred') { |v| opt[:dry_run] = true }
-parser.on('--size-only', 'skip files that match in size') { |v| opt[:size_only] = true }
+parser.on('-c', '--checksum', 'skip based on checksum, not mod-time & size') { |v| opt[:checksum] = true }
+parser.on('-a', '--archive', 'archive mode; same as -rt') { opt[:archive] = true }
 parser.on('-r', '--recursive', 'recurse into directories') { |v| opt[:recursive] = true }
 parser.on('-t', '--times', 'preserve time') { opt[:preserve_time] = true }
-parser.on('-a', '--archive', 'archive mode; same as -rt') { opt[:archive] = true }
+parser.on('-n', '--dry-run', 'show what would have been transferred') { |v| opt[:dry_run] = true }
+parser.on('--delete', 'delete extraneous files from dest dirs') { |v| opt[:delete] = true }
+parser.on('--size-only', 'skip files that match in size') { |v| opt[:size_only] = true }
 parser.separator('')
 parser.separator('SRC, DEST')
 parser.separator('    Use \'googledrive://\' to specify directory on Google Drive.')
 parser.separator('    Example:')
-parser.separator('        gdsync ./sourcedir/ googledrive://destdir')
-parser.separator('        gdsync googledrive://sourcedir/ ./destdir')
-parser.separator('        gdsync googledrive://sourcedir googledrive://destdir')
+parser.separator('        gdsync -a ./sourcedir/ googledrive://destdir')
+parser.separator('        gdsync -a googledrive://sourcedir/ ./destdir')
+parser.separator('        gdsync -a googledrive://sourcedir googledrive://destdir')
 args = parser.parse!(ARGV)
 
 raise "Too few arguments" if args.size < 2
